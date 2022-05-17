97bd5f27b0a3f1161a4c890e5dc6e8e893df00e8
Gauri Joshi <gauri728@gmail.com>
2022-05-17 15:57:16 -0700

    feat/Cell class, test/exists and has attributes

---
+++ lib/cell.rb
@@ -0,0 +1,9 @@
+class Cell
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = ship
+  end
+
+end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,9 +1,15 @@
 require './lib/ship'
 require './lib/cell'
 
-RSpec.describe.Cell do
+RSpec.describe Cell do
   it "exists" do
-  cell = Cell.new("B4")
-  expect(cell).to be_a(Cell)
+    cell = Cell.new("B4")
+    expect(cell).to be_a(Cell)
+  end
+
+  it "has attributes" do
+    cell = Cell.new("B4")
+    expect(cell.coordinate).to eq("B4")
+    expect(cell.ship).to eq(nil)
   end
 end
