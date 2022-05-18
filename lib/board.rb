d326f6c4a5ab081b02bb6334bee40156128f0faf
Gauri Joshi <gauri728@gmail.com>
2022-05-18 16:55:36 -0700

    Feat/cells hash and Test/it has a hash

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,19 @@
+
+
 class Board
   attr_reader :cell, :ship
+
   def initialize
   @cell = cell
   @ship = ship
+  end
+
+  def cells
+  cell_hash = {
+    "A1" => @cell,
+    "A2" => @cell,
+    "A3" => @cell,
+    "A4" => @cell
+    }
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Board do
     board = Board.new
     expect(board).to be_a(Board)
   end
-
+  it "contains a hash" do
+    board = Board.new
+    cell_hash = {
+      "A1" => @cell,
+      "A2" => @cell,
+      "A3" => @cell,
+      "A4" => @cell
+      }
+    expect(board.cells).to eq(cell_hash)
   end
+end
