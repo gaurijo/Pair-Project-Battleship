cc140037a6c464b347be981e68d0d5e037f7bd32
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-18 11:05:23 -0700

    Test/added test for string rep of the cell when board is printed

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -35,4 +35,25 @@ RSpec.describe Cell do
     expect(cell.ship.health).to eq 2
     expect(cell.fired_upon?).to eq true
   end
+
+  it "can return a string representation of the cell when the board is printed" do
+    cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_1.render
+    cell_1.fire_upon
+    expect(cell_1.render).to eq("M")
+
+    cell_2.place_ship(cruiser)
+    expect(cell_2.render).to eq (".")
+    expect(cell_2.render(true)).to eq ("S")
+    cell_2.fire_upon
+    expect(cell_2.render).to eq("H")
+    expect(cruiser.sunk?).to eq false
+    cruiser.hit
+    cruiser.hit
+    expect(cruiser.sunk?).to eq true
+    expect(cell_2.render).to eq ("X")
+  end
 end
