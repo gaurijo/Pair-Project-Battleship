23a571a24ffc3a88aeec34efb361770e501ce3b8
Gauri Joshi <gauri728@gmail.com>
2022-05-18 17:00:48 -0700

    Feat/hash has 16 key value pairs

--- lib/board.rb
+++ lib/board.rb
@@ -13,7 +13,19 @@ class Board
     "A1" => @cell,
     "A2" => @cell,
     "A3" => @cell,
-    "A4" => @cell
+    "A4" => @cell,
+    "B1" => @cell,
+    "B2" => @cell,
+    "B3" => @cell,
+    "B4" => @cell,
+    "C1" => @cell,
+    "C2" => @cell,
+    "C3" => @cell,
+    "C4" => @cell,
+    "D1" => @cell,
+    "D2" => @cell,
+    "D3" => @cell,
+    "D4" => @cell
     }
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,7 +13,19 @@ RSpec.describe Board do
       "A1" => @cell,
       "A2" => @cell,
       "A3" => @cell,
-      "A4" => @cell
+      "A4" => @cell,
+      "B1" => @cell,
+      "B2" => @cell,
+      "B3" => @cell,
+      "B4" => @cell,
+      "C1" => @cell,
+      "C2" => @cell,
+      "C3" => @cell,
+      "C4" => @cell,
+      "D1" => @cell,
+      "D2" => @cell,
+      "D3" => @cell,
+      "D4" => @cell
       }
     expect(board.cells).to eq(cell_hash)
   end
