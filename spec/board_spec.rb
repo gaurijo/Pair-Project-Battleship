634ea56181aeff80aac5449e66104455f094617b
Gauri Joshi <gauri728@gmail.com>
2022-05-19 12:52:05 -0700

    Test/can tell if ship placement is valid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -29,4 +29,16 @@ RSpec.describe Board do
     board.cells["A22"]
     expect(board.valid_coordinate?).to eq(false)
   end
+
+  it "can tell if the placement of the ship is valid" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    board.cells["A1, A2"]
+    expect(board.valid_coordinate?.count).to eq(2)
+    expect(board.valid_placement?(cruiser,["A1", "A2"])).to eq(false)
+    board.cells["A2", "A3", "A4"]
+    expect(board.valid_coordinate?.count).to eq(3)
+    expect(board.valid_placement?(submarine,["A2", "A3", "A4"])).to eq(false)
+  end
 end
