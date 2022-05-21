26fdf678473ac0431e2765b6c5f2fa55ef27d440
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-21 12:03:12 -0700

    Test/added test for placing ship on board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -62,4 +62,17 @@ RSpec.describe Board do
     expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
+
+  it "can place a ship on the board" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+    expect(cell_1.ship).to eq ("Cruiser")
+    expect(cell_2.ship).to eq ("Cruiser")
+    expect(cell_3.ship).to eq ("Cruiser")
+    expect(cell_3.ship == cell_2.ship).to eq true
+  end
 end
