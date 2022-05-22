1d57ddfc8aa4d3783523635d0c7f995a9d213193
Gauri Joshi <gauri728@gmail.com>
2022-05-22 16:28:40 -0700

    Define user setup and print empty board

--- lib/game.rb
+++ lib/game.rb
@@ -18,7 +18,7 @@ class Game
     input = gets.chomp
 
     if input == "p"
-      "Lessssgooooo!"
+      puts "Lessssgooooo!"
     elsif input == "q"
       puts "Boo, quitter!"
     else
@@ -26,7 +26,26 @@ class Game
 
     end
   end
+  def user_setup
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+  end
+
+  def computer_setup
+    cell = Cell.new
+    puts cell.place_ship
+##I want it to place a ship on coordinates, but randomly. how?
+
+  end
 end
 
 game = Game.new
 puts game.start_game
+
+puts game.user_setup
+board = Board.new
+puts board.render
+
+puts "Enter the squares for the Cruiser (3 spaces): "
+square = gets.chomp
