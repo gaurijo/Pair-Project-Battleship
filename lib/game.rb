b60c8d9e205ed62a05b2ac400cde0a5d5f73b6fd
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-23 22:10:21 -0700

    Feat/added random computer placement, start game, and player turn methods

--- lib/game.rb
+++ lib/game.rb
@@ -11,14 +11,14 @@ class Game
     @cruiser_computer = Ship.new("Cruiser", 3)
     @submarine_computer = Ship.new("Submarine", 2)
     @cruiser_player = Ship.new("Cruiser", 3)
-    # @computer_player = Player.new(:computer)
+    @submarine_player = Ship.new("Submarine", 2)
   end
 
   def start_menu
    puts "=========================================="
-   puts
    puts "         Welcome to BATTLESHIP"
    puts "   Enter 'p' to play. Press 'q' to quit."
+   puts "=========================================="
     input = gets.chomp.downcase
     until input == 'p'|| input == 'q'
       puts "Please try again!"
@@ -33,54 +33,56 @@ class Game
     end
   end
 
+  def random_computer_placement(ship)
+    random_cells = @computer_board.cells.keys.sample(ship.length)
+    until @computer_board.valid_placement?(ship, random_cells)
+      random_cells = @computer_board.cells.keys.sample(ship.length)
+    end
+    random_cells
+  end
+
   def start_game
-    @computer_board.place(@cruiser_computer, ["A1", "A2", "A3"])
+    cells = random_computer_placement(@cruiser_computer)
+    @computer_board.place(@cruiser_computer, cells)
+    cells = random_computer_placement(@submarine_computer)
+    @computer_board.place(@submarine_computer, cells)
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long."
-    puts "~~~~~COMPUTER BOARD~~~~~"
-    @computer_board.render(ship_on_cell = false)
+    @player_board.render(ship_on_cell = true)
     puts "Enter the squares for the Cruiser (3 spaces):"
-    @player_board.place(@cruiser_player, ["B1", "B2", "B3"])
+    player_placement = gets.chomp.upcase
+    player_placement.split(" ")
+    @player_board.place(@cruiser_player, player_placement.split(" "))
+    @player_board.render(ship_on_cell = true)
+    puts "Enter the squares for the Submarine (2 spaces):"
+    player_placement = gets.chomp.upcase
+    player_placement.split(" ")
+    @player_board.place(@submarine_player, player_placement.split(" "))
     puts "~~~~~PLAYER BOARD~~~~~"
     @player_board.render(ship_on_cell = true)
   end
 
-    def player_turn
+  def player_turn
     puts "Enter the coordinate for your shot:"
     player_turn_input = gets.chomp.upcase
-    puts "~~~~~COMPUTER BOARD~~~~~"
-    return @computer_board.render(ship_on_cell = false)
-    puts "~~~~~PLAYER BOARD~~~~~"
-    @player_board.render(ship_on_cell = true)
     if @computer_board.valid_coordinate?(player_turn_input)
-
-
-
-
-
-    # computer_turn_input = ["C1"]
-      # if @player_board.valid_coordinate?(computer_turn_input)
-    # require 'pry'; binding.pry
-        end
-      end
+      @computer_board.cells[player_turn_input].fire_upon
+      puts "~~~~~COMPUTER BOARD~~~~~"
+      @computer_board.render
+      puts "~~~~~PLAYER BOARD~~~~~"
+      @player_board.render(ship_on_cell = true)
     end
-
-
-
-
-
+  end
+end
 
     # A single turn consists of:
-    # Displaying the boards - done
-    # Player choosing a coordinate to fire on - done
+    # Displaying the boards
+    # Player choosing a coordinate to fire on
     # Computer choosing a coordinate to fire on
     # Reporting the result of the Player’s shot
     # Reporting the result of the Computer’s shot
 
-
-
-
 battleship = Game.new
 battleship.start_menu
-battleship.turn
+battleship.player_turn
