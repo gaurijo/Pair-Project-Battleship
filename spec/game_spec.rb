020e564918747e85ac2ab2dc6f7cd75dc6ebe70b
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-25 14:35:52 -0700

    Removed tests that required user input and radomization

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe Game do
     @board = Board.new
     @computer_board = Board.new
     @player_board = Board.new
+    @cruiser_player = Ship.new("Cruiser", 3)
+    @submarine_player = Ship.new("Submarine", 2)
+    @cruiser_computer = Ship.new("Cruiser", 3)
+    @submarine_computer = Ship.new("Submarine", 2)
   end
 
   it "exists" do
@@ -17,58 +21,19 @@ RSpec.describe Game do
 
   it "can contain a computer board" do
     expect(@computer_board).to be_a(Board)
-
   end
 
   it "can contain a player board" do
     expect(@player_board).to be_a(Board)
   end
 
-   it "can contain a player ship" do
-    ship = Ship.new("Cruiser", 3)
+  it "can contain a player ship" do
     expect(@cruiser_player).to be_a(Ship)
     expect(@submarine_player).to be_a(Ship)
   end
 
-  xit "can contain a computer ship" do
+  it "can contain a computer ship" do
     expect(@cruiser_computer).to be_a(Ship)
     expect(@submarine_computer).to be_a(Ship)
   end
-
-  xit "can take a turn" do
-  end
-
-  xit "can keep track of hits, misses, and sinks and display them" do
-  end
-
-  xit "can determine a winner" do
-
-
-  end
-
-  it "can contain a player board" do
-    expect(@player_board).to be_a(Board)
-  end
-
-   it "can contain a player ship" do
-    ship = Ship.new("Cruiser", 3)
-    expect(@cruiser_player).to be_a(Ship)
-    expect(@submarine_player).to be_a(Ship)
-  end
-
-  xit "can contain a computer ship" do
-    expect(@cruiser_computer).to be_a(Ship)
-    expect(@submarine_computer).to be_a(Ship)
-  end
-
-  xit "can take a turn" do
-  end
-
-  xit "can keep track of hits, misses, and sinks and display them" do
-  end
-
-  xit "can determine a winner" do
-
-  end
-
 end
