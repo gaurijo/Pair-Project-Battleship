cb571571b8f565a9399fa2339e7fdf5acdc679de
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-24 22:41:40 -0700

    Fix/ fixed render test, had order incorrect

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,11 +46,9 @@ RSpec.describe Board do
   end
 
  it "can tell if the placement of the ship is valid" do
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
-    expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+    expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to eq(true)
+    expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to eq(true)
+  end
 
   it "can tell if the placement of the ship is valid" do
     expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to eq(true)
@@ -77,7 +75,7 @@ RSpec.describe Board do
   it "can render the board" do
     @board.place(@cruiser, ["A1", "A2", "A3"])
 
-    expect(@board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
-    expect(@board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
+    expect(@board.render).to eq("  1 2 3 4 \nA . . . ."+"\nB . . . ."+"\nC . . . ."+"\nD . . . .")
+    expect(@board.render(true)).to eq("  1 2 3 4 \nA S S S ."+"\nB . . . ."+"\nC . . . ."+"\nD . . . .")
   end
 end
